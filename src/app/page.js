e0bfd9c276769ff17fe54a77fdb55fcd54f15d95
Awais Jawaid <m.awaisjawaid@gmail.com>
2026-04-04 20:03:10 +0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold text-red-600">
+        MJ Logistics Website Coming Soon
+      </h1>
+    </main>
+  );
+}
